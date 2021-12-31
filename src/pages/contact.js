0c1f7422e6bc795e9268770c6e9906f7ae61045e
Front End Developer <manasiry3@gmail.com>
2021-12-31 17:13:43 +0530

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import emailjs from 'emailjs-com'
 import { init } from '@emailjs/browser';
 import Layout from '../components/layout';
-import ReCAPTCHA from 'react-google-recaptcha';
 
 init("user_ASXLmERL5ByrsNmHHTr9A");
 
@@ -12,7 +11,7 @@ const Contact = () => {
     const [message, setMessage] = useState('');
     const [emailSent, setEmailSent] = useState(false);
 
-    const submit = (captchaValue) => {
+    const submit = () => {
         if (name && email && message) {
             const serviceId = 'service_9v5qyj8';
             const templateId = 'template_t3frd2p';
@@ -20,8 +19,7 @@ const Contact = () => {
             const templateParams = {
                 name,
                 email,
-                message,
-                 'g-recaptcha-response': captchaValue,
+                message
             };
 
             emailjs.send(serviceId, templateId, templateParams, userId)
@@ -46,10 +44,6 @@ const Contact = () => {
                             <input type="text" placeholder="Your Name" value={name} onChange={e => setName(e.target.value)} />
                             <input type="email" placeholder="Your email address" value={email} onChange={e => setEmail(e.target.value)} />
                             <textarea placeholder="Your message" value={message} onChange={e => setMessage(e.target.value)}></textarea>
-                            <ReCAPTCHA
-                               sitekey={process.env.CAPTCHA_SITE_KEY}
-                               onChange={submit}
-                            />
                             <button onClick={submit}>Send Message</button>
 
                             <span className={emailSent ? 'visible' : null}>Thank you for your message, we will be in touch in no time!</span>
