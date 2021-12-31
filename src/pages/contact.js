07eae1729976b33660ee423f6c33ebc01b8e0303
Front End Developer <manasiry3@gmail.com>
2021-12-31 16:08:14 +0530

    contact changes

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,7 @@
 import React, { useState } from 'react'
 import emailjs from 'emailjs-com'
 import { init } from '@emailjs/browser';
-import ReCAPTCHA from 'react-google-recaptcha';
 import Layout from '../components/layout';
-
-
 init("user_ASXLmERL5ByrsNmHHTr9A");
 
 const Contact = () => {
@@ -13,7 +10,7 @@ const Contact = () => {
     const [message, setMessage] = useState('');
     const [emailSent, setEmailSent] = useState(false);
 
-    const submit = (captchaValue) => {
+    const submit = () => {
         if (name && email && message) {
             const serviceId = 'service_9v5qyj8';
             const templateId = 'template_t3frd2p';
@@ -21,8 +18,7 @@ const Contact = () => {
             const templateParams = {
                 name,
                 email,
-                message,
-                'g-recaptcha-response': captchaValue,
+                message
             };
 
             emailjs.send(serviceId, templateId, templateParams, userId)
@@ -36,23 +32,25 @@ const Contact = () => {
         } else {
             alert('Please fill in all fields.');
         }
-
     }
 
     return (
         <Layout>
-            <div id="contact-form" className='container my-5'>
-                <input type="text" placeholder="Your Name" value={name} onChange={e => setName(e.target.value)} />
-                <input type="email" placeholder="Your email address" value={email} onChange={e => setEmail(e.target.value)} />
-                <textarea placeholder="Your message" value={message} onChange={e => setMessage(e.target.value)}></textarea>
-                <ReCAPTCHA
-                    sitekey={process.env.CAPTCHA_SITE_KEY}
-                    onChange={submit}
-                />
-                <button onClick={submit}>Send Message</button>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-md-12'>
+                        <div id="contact-form">
+                            <input type="text" placeholder="Your Name" value={name} onChange={e => setName(e.target.value)} />
+                            <input type="email" placeholder="Your email address" value={email} onChange={e => setEmail(e.target.value)} />
+                            <textarea placeholder="Your message" value={message} onChange={e => setMessage(e.target.value)}></textarea>
+                            <button onClick={submit}>Send Message</button>
 
-                <span className={emailSent ? 'visible' : null}>Thank you for your message, we will be in touch in no time!</span>
+                            <span className={emailSent ? 'visible' : null}>Thank you for your message, we will be in touch in no time!</span>
+                        </div>
+                    </div>
+                </div>
             </div>
+
         </Layout>
     );
 };
