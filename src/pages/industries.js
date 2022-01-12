6ecc085998516df48e3a607b28ea33b915c75841
Front End Developer <manasiry3@gmail.com>
2022-01-12 23:18:22 +0530

    industries page update

--- src/pages/industries.js
+++ src/pages/industries.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from '../components/layout';
 import Seo from "../components/seo";
-import { useStaticQuery, graphql, Link } from "gatsby";
+import { useStaticQuery, graphql } from "gatsby";
 import Accordion from '../utils/Accordion';
 
 const getdata = graphql`
@@ -22,6 +22,56 @@ const getdata = graphql`
           }
           mainsubcategorypara
         }
+        pharmaceuticals {
+          mainsubcategorypara
+          mainsubcategorylink {
+            ... on WPGraphQL_Page {
+              id
+              content
+            }
+          }
+          mainsubcategoryheading
+        }
+        healthcareMedicalDevices {
+          mainsubcategorypara
+          mainsubcategorylink {
+            ... on WPGraphQL_Page {
+              id
+              content
+            }
+          }
+          mainsubcategoryheading
+        }
+        consumerGoodsServices {
+          mainsubcategorypara
+          mainsubcategorylink {
+            ... on WPGraphQL_Page {
+              id
+              content
+            }
+          }
+          mainsubcategoryheading
+        }
+        chemicalIndustrial {
+          mainsubcategorypara
+          mainsubcategorylink {
+            ... on WPGraphQL_Page {
+              id
+              content
+            }
+          }
+          mainsubcategoryheading
+        }
+        biotechnology {
+          mainsubcategoryheading
+          mainsubcategorylink {
+            ... on WPGraphQL_Page {
+              id
+              content
+            }
+          }
+          mainsubcategorypara
+        }
       }
     }
   }
@@ -30,6 +80,11 @@ const getdata = graphql`
 const Industries = () => {
   const data = useStaticQuery(getdata);
   const common = data.wpgraphql.page.industries.maincategory;
+  const pharmaceuticals = data.wpgraphql.page.industries.pharmaceuticals;
+  const healthcareMedicalDevices = data.wpgraphql.page.industries.healthcareMedicalDevices;
+  const consumerGoodsServices = data.wpgraphql.page.industries.consumerGoodsServices;
+  const chemicalIndustrial = data.wpgraphql.page.industries.chemicalIndustrial;
+  const biotechnology = data.wpgraphql.page.industries.biotechnology;
 
   return (
     <Layout>
@@ -40,25 +95,129 @@ const Industries = () => {
             <div className='text-center'>
               <h1 className='display-3 mb-5'>Industries</h1>
             </div>
-            <div className="container industries-outr">
-              <div className='row'>
-                {common.map((i, index) => (
-                  <div className='mb-5' key={index}>
-                    <article>
-                      <>
-                        <h4>{i.mainsubcategoryheading}</h4>
-                        <p>{i.mainsubcategorypara} </p>
-                        <Accordion content={<div dangerouslySetInnerHTML={{ __html: i.mainsubcategorylink.content }} />} />
-                      </>
-                    </article>
-                  </div>
-                ))}
-              </div>
+          </div>
+        </div>
+      </div>
+      <section className='mainSpacing'>
+        <div className="container industries-outr">
+          <div className='row'>
+            <div className='col-md-12'>
+              <h1 className='display-5 mb-3 border-bottom d-inline-block pb-3'>Electronics and High Tech</h1>
+              {common.map((i, index) => (
+                <div className='mb-5' key={index}>
+                  <article>
+                    <>
+                      <h4>{i.mainsubcategoryheading}</h4>
+                      <p>{i.mainsubcategorypara} </p>
+                      <Accordion content={<div dangerouslySetInnerHTML={{ __html: i.mainsubcategorylink.content }} />} />
+                    </>
+                  </article>
+                </div>
+              ))}
             </div>
           </div>
         </div>
-
-      </div>
+      </section>
+      <section className='mainSpacing'>
+        <div className="container industries-outr">
+          <div className='row'>
+            <div className='col-md-12'>
+              <h1 className='display-5 mb-3 border-bottom d-inline-block pb-3'>Biotechnology</h1>
+              {pharmaceuticals.map((i, index) => (
+                <div className='mb-5' key={index}>
+                  <article>
+                    <>
+                      <h4>{i.mainsubcategoryheading}</h4>
+                      <p>{i.mainsubcategorypara} </p>
+                      <Accordion content={<div dangerouslySetInnerHTML={{ __html: i.mainsubcategorylink.content }} />} />
+                    </>
+                  </article>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='mainSpacing'>
+        <div className="container industries-outr">
+          <div className='row'>
+            <div className='col-md-12'>
+              <h1 className='display-5 mb-3 border-bottom d-inline-block pb-3'>Pharmaceuticals</h1>
+              {healthcareMedicalDevices.map((i, index) => (
+                <div className='mb-5' key={index}>
+                  <article>
+                    <>
+                      <h4>{i.mainsubcategoryheading}</h4>
+                      <p>{i.mainsubcategorypara} </p>
+                      <Accordion content={<div dangerouslySetInnerHTML={{ __html: i.mainsubcategorylink.content }} />} />
+                    </>
+                  </article>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='mainSpacing'>
+        <div className="container industries-outr">
+          <div className='row'>
+            <div className='col-md-12'>
+              <h1 className='display-5 mb-3 border-bottom d-inline-block pb-3'>Chemical &amp; Industrial</h1>
+              {consumerGoodsServices.map((i, index) => (
+                <div className='mb-5' key={index}>
+                  <article>
+                    <>
+                      <h4>{i.mainsubcategoryheading}</h4>
+                      <p>{i.mainsubcategorypara} </p>
+                      <Accordion content={<div dangerouslySetInnerHTML={{ __html: i.mainsubcategorylink.content }} />} />
+                    </>
+                  </article>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='mainSpacing'>
+        <div className="container industries-outr">
+          <div className='row'>
+            <div className='col-md-12'>
+              <h1 className='display-5 mb-3 border-bottom d-inline-block pb-3'>Consumer Goods &amp; Services</h1>
+              {chemicalIndustrial.map((i, index) => (
+                <div className='mb-5' key={index}>
+                  <article>
+                    <>
+                      <h4>{i.mainsubcategoryheading}</h4>
+                      <p>{i.mainsubcategorypara} </p>
+                      <Accordion content={<div dangerouslySetInnerHTML={{ __html: i.mainsubcategorylink.content }} />} />
+                    </>
+                  </article>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className='mainSpacing'>
+        <div className="container industries-outr">
+          <div className='row'>
+            <div className='col-md-12'>
+              <h1 className='display-5 mb-3 border-bottom d-inline-block pb-3'>Healthcare &amp; Medical Devices</h1>
+              {biotechnology.map((i, index) => (
+                <div className='mb-5' key={index}>
+                  <article>
+                    <>
+                      <h4>{i.mainsubcategoryheading}</h4>
+                      <p>{i.mainsubcategorypara} </p>
+                      <Accordion content={<div dangerouslySetInnerHTML={{ __html: i.mainsubcategorylink.content }} />} />
+                    </>
+                  </article>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 };
