eafc2db0d0f762fe463c6b687641e7a8a090374b
Chon Nguyen <chonnh@gmail.com>
2022-01-20 17:06:31 +0700

    Convert Patent FAQs subpages to gatsby

--- src/pages/patentfaqs.js
+++ src/pages/patentfaqs.js
@@ -35,6 +35,28 @@ const getdata = graphql`
 const Faq = () => {
     const data = useStaticQuery(getdata);
     const common = data.wpgraphql.page;
+    console.log(common.content)
+    const replaceWPUrl = (content) => {
+      // check server side rendering
+      // https://github.com/gatsbyjs/gatsby/issues/19487
+      if (typeof window === 'undefined' || !window.document) {
+        return;
+      }
+      // Remove part
+      const WP_URL = '';
+      // create blog content obj
+      var contentObj = document.createElement( 'div' );
+      contentObj.innerHTML = content;
+
+      // get all a href
+      var aTags = contentObj.getElementsByTagName('a');
+      for (const a of aTags) {
+        a.setAttribute("href", a.href.replace(/^.*\/\/[^\/]+/, ''));
+      }
+
+      return contentObj.innerHTML
+    }
+    //
     return (
         <Layout>
             <Seo title="Faq" description={common.seo.metaDesc} canonical={common.uri} seo={common.seo}  />
@@ -45,7 +67,7 @@ const Faq = () => {
                             <h1 className='display-3 mb-5'>Patent FAQs</h1>
                         </div>
                         <div className="container">
-                            <div className='faqs' dangerouslySetInnerHTML={{ __html: common.content }} />
+                            <div className='faqs' dangerouslySetInnerHTML={{ __html: replaceWPUrl(common.content) }} />
                         </div>
                     </div>
                 </div>
---
+++ src/pages/patentfaqs/utility-patent-application/filing-a-utility-patent-application.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import Layout from '../../../components/layout';
+import Seo from "../../../components/seo";
+import { useStaticQuery, graphql } from "gatsby";
+
+
+const getdata = graphql`
+{
+  wpgraphql {
+    page(id: "cG9zdDo4NzA4") {
+      id
+      title
+      content
+      uri
+      seo {
+        canonical
+        metaDesc
+        metaKeywords
+        title
+        twitterTitle
+        twitterDescription
+        opengraphDescription
+        opengraphPublishedTime
+        opengraphModifiedTime
+        opengraphTitle
+        opengraphType
+        opengraphImage {
+          sourceUrl
+        }
+      }    
+    }
+  }
+}
+  `
+const FilingPatentApplication = () => {
+    const data = useStaticQuery(getdata);
+    const common = data.wpgraphql.page;
+    console.log(common.content)
+    return (
+        <Layout>
+            <Seo title={common.title} description={common.seo.metaDesc} canonical={common.uri} seo={common.seo}  />
+            <div className='container py-5'>
+                <div className='row justify-content-center'>
+                    <div className='col-md-12'>
+                        <div className='text-center'>
+                            <h1 className='display-3 mb-5'>{common.title}</h1>
+                        </div>
+                        <div className="container">
+                            <div className='faqs' dangerouslySetInnerHTML={{ __html: common.content }} />
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </Layout>
+    );
+};
+
+export default FilingPatentApplication;
