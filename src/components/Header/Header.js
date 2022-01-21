e2db8006a94cfa735230ebab2cdc8d36b8911cea
Front End Developer <manasiry3@gmail.com>
2022-01-21 21:56:17 +0530

    design changes of about

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -49,7 +49,7 @@ const Header = () => {
                 </Navbar.Brand>
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mx-auto">
-                        {datamenu.map(menus => !menus.parentId && !menus.childItems.nodes.length ? 
+                        {datamenu.map(menus => !menus.parentId && !menus.childItems.nodes.length ?
                             <Nav.Link
                                 href={menus.path}
                                 key={menus.id}
@@ -57,7 +57,7 @@ const Header = () => {
                                 onSelect={(selectedKey) => setActive(selectedKey)}>
                                 {menus.label}
                             </Nav.Link>
-                            : menus.childItems.nodes.length > 0 ? 
+                            : menus.childItems.nodes.length > 0 ?
                                 <NavDropdown title={menus.label}>
                                     {menus.childItems.nodes.map(childItem => (
                                         <NavDropdown.Item href={childItem.path}>
@@ -65,13 +65,13 @@ const Header = () => {
                                         </NavDropdown.Item>
                                     ))}
                                 </NavDropdown>
-                            : null
+                                : null
                         )}
                     </Nav>
                 </Navbar.Collapse>
                 <div className='ms-auto d-flex small-device-header'>
                     <div className='telephone d-flex align-items-center me-3'><FontAwesomeIcon icon={faPhoneSquareAlt} size="2x" style={{ color: "#fff" }} /><a href="tel:8002343032" className='ms-2'> <span>800-234-3032</span></a></div>
-                    <Link className='btn btn-outline-danger' to="https://calendly.com/patentpc/one-on-one-with-attorney-tran" target="_blank">Book Free Intoductory Call</Link>
+                    <Link className='btn btn-outline-warning' to="https://calendly.com/patentpc/one-on-one-with-attorney-tran" target="_blank">Book Free Intoductory Call</Link>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" className='ms-2' />
                 </div>
             </Container>
