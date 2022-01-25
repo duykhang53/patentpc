c2f4fb1aa18ab8ac8ae7c2ca55b875f3c55c4777
Chon Nguyen <chonnh@gmail.com>
2022-01-25 15:21:37 +0700

    add footer menu

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import { useStaticQuery, graphql } from "gatsby";
+import Nav from 'react-bootstrap/Nav';
 import Logo from "../../images/logo.png";
 
 const Footer = () => {
@@ -12,6 +13,25 @@ const Footer = () => {
           node {
             publicURL
             name
+          }
+        }
+      }
+      wpMenu(id: { eq: "dGVybToxNg==" }) {
+        menuItems {
+          nodes {
+            label
+            url
+            parentId
+            id
+            path
+            childItems {
+              nodes {
+                url
+                label
+                id
+                path
+              }
+            }
           }
         }
       }
@@ -26,7 +46,15 @@ const Footer = () => {
                             <Link to="/" className='mb-2 d-inline-block'>
                                 <img src={Logo} alt="PatentPc" className="logo" width="150" height="33" />
                             </Link>
-
+                            <ul className='list-inline'>
+                              {data.wpMenu.menuItems.nodes.map((menu, index, menus) => ((!menu.parentId && !menu.childItems.nodes.length) || (menu.parentId)) ?
+                                <li className='list-inline-item'>
+                                  <a href={menu.path} className='text-muted text-decoration-none'>
+                                      {menu.label}
+                                  </a>  { (index + 1 === menus.length) ? null : <span className='text-muted'> &nbsp; </span> }
+                                </li> : null
+                              )}
+                            </ul>
                             <ul className='list-inline'>
                                 <p className="list-inline-item text-muted">&copy; 2022 Patent, PC</p>
                                 {data.allFile.edges.map((file, index) => {
